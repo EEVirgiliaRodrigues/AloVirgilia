1b5655a64eb377199aa8e1f9d1226d6baa933b2c
EEVirgiliaRodrigues <gabrielpetryla@gmail.com>
2026-04-08 08:31:43 -0300

    novos usuarios

--- lib/usuarios.ts
+++ lib/usuarios.ts
@@ -9,7 +9,33 @@ export interface Usuario {
 }
 
 export const usuarios: Usuario[] = [
-  { email: 'gabrielmelnic@prof.educacao.sp.gov.br', nome: 'Gabriel' },
+  { email: 'gabrielmelnic@prof.educacao.sp.gov.br', nome: 'Prof Gabriel' },
+  { email: '00001159958555SP@al.educacao.sp.gov.br', nome: 'Anne' },
+  { email: '00001142592376SP@al.educacao.sp.gov.br', nome: 'Gabryelle' },
+  { email: '00001248842790sp@al.educacao.sp.gov.br', nome: 'Gabriel' },
+  { email: '00001160742753sp@al.educacao.sp.gov.br', nome: 'Gabriela' },
+  { email: '00001121670180SP@al.educacao.sp.gov.br', nome: 'Pedro Domingues' },
+  { email: '00001141154547SP@al.educacao.sp.gov.br', nome: 'Rebeca' },
+  { email: '00001135998218SP@al.educacao.sp.gov.br', nome: 'Samara' },
+  { email: '00001125102779sp@al.educacao.sp.gov.br', nome: 'Pedro Kleber' },
+  { email: '0000112599020XSP@al.educacao.sp.gov.br', nome: 'Davi Oliveira' },
+  { email: '00001132843078sp@al.educacao.sp.gov.br', nome: 'Marina' },
+  { email: '00001130462353SP@al.educacao.sp.gov.br', nome: 'Lucas Araújo' },
+  { email: '00001100657915sp@al.educacao.sp.gov.br', nome: 'Pedro Gabriel' },
+  { email: '00001264262498sp@al.educacao.sp.gov.br', nome: 'Yajaira' },
+  { email: '00001135284477SP@al.educacao.sp.gov.br', nome: 'Sara' },
+  { email: '00001153223466sp@al.educacao.sp.gov.br', nome: 'Nayber' },
+  { email: '00001118142895sp@al.educacao.sp.gov.br', nome: 'Lara' },
+  { email: '00001116940462sp@al.educacao.sp.gov.br', nome: 'Isadora' },
+  { email: '00001140192498sp@al.educacao.sp.gov.br', nome: 'Samuel' },
+  { email: '00001118837836sp@al.educacao.sp.gov.br', nome: 'Davi Emannuel' }
+  { email: '00001118960828sp@al.educacao.sp.gov.br', nome: 'Iasmin' }
+  { email: '00001128555050sp@al.educacao.sp.gov.br', nome: 'Edson' }
+  { email: '00001115107872sp@al.educacao.sp.gov.br', nome: 'Vinicius' }
+  { email: '00001125539318sp@al.educacao.sp.gov.br', nome: 'Débora' }
+  { email: '00001125854479sp@al.educacao.sp.gov.br', nome: 'Jhonatan' }
+  { email: '00001118131010sp@al.educacao.sp.gov.br', nome: 'João Paulo' }
+  
   // Adicione mais emails aqui:
   // { email: 'professor@escola.edu.br', nome: 'Professor Silva' },
   // { email: 'aluno@escola.edu.br', nome: 'Aluno João' },
